4e13b455c39b11c56009a3e5560037f10d75fb87
Jerlylan <jerlylan001858@gmail.com>
2026-05-11 22:12:29 -0300

    corrige url correta do supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hcqqrahbbthwpzecusv.supabase.co";
+const supabaseUrl = "https://hcqrqahbbthwpzecusvs.supabase.co";
 
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcXJxYWhiYnRod3B6ZWN1c3ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MzkzNDUsImV4cCI6MjA5NDExNTM0NX0.HqK3AcFT9pC2TQQ4LkNJrn5ndq7LTVJDW3eRA2Wbs7E";
 
